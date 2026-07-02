db040857be2b44a7e00e697895e296941174aa25
Elias Busch <49643239+Elrabu@users.noreply.github.com>
2026-07-02 22:10:12 +0200

    Update memory_tracker.py

--- memory_tracker.py
+++ memory_tracker.py
@@ -5,30 +5,30 @@ import threading
 import psutil
 
 class MemoryTracker:
-    def __init__(self):
-        self._process = psutil.Process(os.getpid())
-        self._peak_mb: float = self._current_rss_mb()
+    def __init__(self): 
+        self._process = psutil.Process(os.getpid()) #get the psutil handle for the current process
+        self._peak_mb: float = self._current_rss_mb() #get the current RSS values as a starting point
 
     def _current_rss_mb(self) -> float:
-        return self._process.memory_info().rss / (1024 * 1024)
+        return self._process.memory_info().rss / (1024 * 1024) #calculate current RSS value in mb
 
     def _update_peak(self):
-        current = self._current_rss_mb()
-        if current > self._peak_mb:
-            self._peak_mb = current
+        current = self._current_rss_mb() #check current memory usage
+        if current > self._peak_mb: #set current value variable if current value is higher than the peak
+            self._peak_mb = current #update peak value
 
-    def start_peak_sampler(self, interval_s: float = 0.25):
+    def start_peak_sampler(self, interval_s: float = 0.25): #start periodic thread sampling
         def _sample():
             while True:
-                self._update_peak()
+                self._update_peak() #measure peak through the corresponding function
                 time.sleep(interval_s)
-        threading.Thread(target=_sample, daemon=True, name="mem-sampler").start()
+        threading.Thread(target=_sample, daemon=True, name="mem-sampler").start() #start new thread and mae sure that it is stopped on program end
 
-    def print_summary(self):
+    def print_summary(self): #print the peak memory usage value
         """Gibt nur den Peak-Speicher aus."""
         self._update_peak()
         print("################################################################")
         print(f"Peak Memory (RSS): {self._peak_mb:>10.2f} MB")
         print("################################################################")
 
-memory = MemoryTracker()
+memory = MemoryTracker() # register the class as a global Singleton, so that it can be used in the whole project
